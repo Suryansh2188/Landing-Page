f3c6621eea6a9d6d0d6a7ebb44dc7282ef90bcdf
Suryansh Soni <119338714+Suryansh2188@users.noreply.github.com>
2025-04-06 00:39:58 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  server: {
-    allowedHosts: ['a5c3-27-60-13-243.ngrok-free.app']
-  }
 })
